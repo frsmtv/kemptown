4bd2afc2f65d15eb11e8cadf3546211c14d89f10
frsmtv <fsimitchiev@gmail.com>
2016-12-05 18:04:29 +0100

    coucou

---
+++ js/main.js
@@ -0,0 +1,45 @@
+jQuery(function($){
+
+
+
+
+    // *********** Invert Scroll ***********
+  $.jInvertScroll(['#city', '#camion', '#clouds', '#theContent']);
+
+
+    // **** Close introMessage ****
+    $("#introMessageClose").click(function(){
+        $("#introMessage").hide();
+    });
+
+    // **** Menu & Close images SWAP ****
+    var imgSwap = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
+
+    $(function () {
+        $('img.imgSwap').hover(imgSwap, imgSwap);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ========================= //
+  }); // == CLOSING JQUERY == //
